7d856bf46c927564aa2563eece9ee2aa2eb2ff03
go1dfish <justageekbiz@gmail.com>
2015-02-03 19:14:37 -0500

    Multiple improvements

--- politic-bot.js
+++ politic-bot.js
@@ -10,23 +10,27 @@ config.userAgent = pkg.name+'/'+pkg.version+' by '+pkg.author;
 
 PoliticBot(config, function(bot) {
   var commentRemovals = PoliticBot.commentRemovals(bot, templates);
-  return RSVP.all([
-    PoliticBot.ingest(bot).eventStream(),
-    PoliticBot.otherDiscussions(bot, templates),
-    PoliticBot.mirrorTopic(bot),
-    PoliticBot.postRemovals(bot, templates),
-    PoliticBot.commander(bot, templates).pollForCommands(null, {
-      check: function(id) {
-        console.log('check', id);
-        if (!id) {return;}
-        if (id.match(/^http/)) {return bot.submitted(undefined, id).then(_.first).then(bot.mirrorPostNow);}
-        return bot.byId(id).then(bot.mirrorPostNow);
-      }
-    }),
-    commentRemovals.pollForRemovals(),
-    commentRemovals.gatherCommentIds(),
-    PoliticBot.schedule.repeat(function() {
-      if (!Object.keys(bot.updateQueue).length) {return bot.fetchMirrors();} return RSVP.resolve();
-    })
-  ]);
+  var ingest = PoliticBot.ingest(bot);
+  return ingest.db.then(function(db) {
+    return RSVP.all([
+      ingest.eventStream(20000, 200000),
+      PoliticBot.otherDiscussions(bot, templates),
+      PoliticBot.mirrorTopic(bot),
+      PoliticBot.postRemovals(bot, templates),
+      PoliticBot.commander(bot, templates).pollForCommands(null, {
+        check: function(id) {
+          console.log('check', id);
+          if (!id) {return;}
+          if (id.match(/^http/)) {return bot.submitted(undefined, id).then(_.first).then(bot.mirrorPostNow);}
+          return bot.byId(id).then(bot.mirrorPostNow);
+        }
+      }),
+      commentRemovals.pollForComments(),
+      commentRemovals.pollForRemovals(),
+      commentRemovals.gatherCommentIds(),
+      PoliticBot.schedule.repeat(function() {
+        if (!Object.keys(bot.updateQueue).length) {return bot.fetchMirrors();} return RSVP.resolve();
+      })
+    ]);
+  });
 });

bbba8e92d7daa74384ad609df4f3e81d93ded640
go1dfish <justageekbiz@gmail.com>
2015-02-10 13:32:05 -0500

    Split ingest into separate modules

--- politic-bot.js
+++ politic-bot.js
@@ -1,5 +1,6 @@
 var RSVP = require('rsvp'), fs = require('fs'), Handlebars = require('handlebars');
 var PoliticBot = require('./lib/main'), _ = require('underscore');
+var snoochives = require('snoochives');
 config = require('./config'), pkg = require('./package'), templates = {};
 config.userAgent = pkg.name+'/'+pkg.version+' by '+pkg.author;
 ['mirror', 'report', 'comment'].forEach(function(name) {
@@ -8,26 +9,28 @@ config.userAgent = pkg.name+'/'+pkg.version+' by '+pkg.author;
   }
 });
 
+process.setMaxListeners(1000);
+
 PoliticBot(config, function(bot) {
-  var commentRemovals = PoliticBot.commentRemovals(bot, templates);
-  var ingest = PoliticBot.ingest(bot);
-  return ingest.db.then(function(db) {
+  bot.data = snoochives(bot.api, 'ingest', {
+    t1: {depth: 500000, extra: ['author']},
+    t3: {depth: 10000, extra: ['url', 'is_self']}
+  }, PoliticBot.schedule);
+
+  return bot.data.promise.then(function() {
     return RSVP.all([
-      ingest.eventStream(10000, 250000),
       PoliticBot.otherDiscussions(bot, templates),
       PoliticBot.mirrorTopic(bot),
       PoliticBot.postRemovals(bot, templates),
       PoliticBot.commander(bot, templates).pollForCommands(null, {
-        check: function(id) {
-          console.log('check', id);
-          if (!id) {return;}
-          if (id.match(/^http/)) {return bot.submitted(undefined, id).then(_.first).then(bot.mirrorPostNow);}
-          return bot.byId(id).then(bot.mirrorPostNow);
+        check: function(id) {if (!id) {return;} console.log('check', id);
+          if (id.match(/^http/)) {return bot.mirrorUrlNow(id);}
+          return bot.byId(id).then(function(post) {
+            return bot.mirrorUrl(post.url);
+          });
         }
       }),
-      commentRemovals.pollForComments(),
-      commentRemovals.pollForRemovals(),
-      commentRemovals.gatherCommentIds(),
+      PoliticBot.commentRemovals(bot, templates),
       PoliticBot.schedule.repeat(function() {
         if (!Object.keys(bot.updateQueue).length) {return bot.fetchMirrors();} return RSVP.resolve();
       })
